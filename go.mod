162b0942e66a463c721e1bbb09a09b4bfdb7c2da
chance <zhiquan911@qq.com>
2020-03-13 22:01:26 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/assetsadapterstore/ausd-adapter
+
+go 1.13
+
+require (
+	github.com/assetsadapterstore/xpay-adapter v1.0.1
+	github.com/astaxie/beego v1.12.0
+	github.com/blocktree/openwallet v1.7.0
+)
---
+++ openwtester/transcation_assets_test.go
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2018 The openwallet Authors
+ * This file is part of the openwallet library.
+ *
+ * The openwallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The openwallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package openwtester
+
+import (
+	"github.com/blocktree/openwallet/openw"
+	"testing"
+	"time"
+
+	"github.com/blocktree/openwallet/log"
+	"github.com/blocktree/openwallet/openwallet"
+)
+
+func testGetAssetsAccountBalance(tm *openw.WalletManager, walletID, accountID string) {
+	balance, err := tm.GetAssetsAccountBalance(testApp, walletID, accountID)
+	if err != nil {
+		log.Error("GetAssetsAccountBalance failed, unexpected error:", err)
+		return
+	}
+	log.Info("balance:", balance)
+}
+
+func testGetAssetsAccountTokenBalance(tm *openw.WalletManager, walletID, accountID string, contract openwallet.SmartContract) {
+	balance, err := tm.GetAssetsAccountTokenBalance(testApp, walletID, accountID, contract)
+	if err != nil {
+		log.Error("GetAssetsAccountTokenBalance failed, unexpected error:", err)
+		return
+	}
+	log.Info("token balance:", balance.Balance)
+}
+
+func testCreateTransactionStep(tm *openw.WalletManager, walletID, accountID, to, amount, feeRate string, contract *openwallet.SmartContract) (*openwallet.RawTransaction, error) {
+
+	//err := tm.RefreshAssetsAccountBalance(testApp, accountID)
+	//if err != nil {
+	//	log.Error("RefreshAssetsAccountBalance failed, unexpected error:", err)
+	//	return nil, err
+	//}
+
+	rawTx, err := tm.CreateTransaction(testApp, walletID, accountID, amount, to, feeRate, "", contract)
+
+	if err != nil {
+		log.Error("CreateTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	return rawTx, nil
+}
+
+func testCreateSummaryTransactionStep(
+	tm *openw.WalletManager,
+	walletID, accountID, summaryAddress, minTransfer, retainedBalance, feeRate string,
+	start, limit int,
+	contract *openwallet.SmartContract) ([]*openwallet.RawTransaction, error) {
+
+	rawTxArray, err := tm.CreateSummaryTransaction(testApp, walletID, accountID, summaryAddress, minTransfer,
+		retainedBalance, feeRate, start, limit, contract)
+
+	if err != nil {
+		log.Error("CreateSummaryTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	return rawTxArray, nil
+}
+
+func testSignTransactionStep(tm *openw.WalletManager, rawTx *openwallet.RawTransaction) (*openwallet.RawTransaction, error) {
+
+	_, err := tm.SignTransaction(testApp, rawTx.Account.WalletID, rawTx.Account.AccountID, "12345678", rawTx)
+	if err != nil {
+		log.Error("SignTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	log.Infof("rawTx: %+v", rawTx)
+	return rawTx, nil
+}
+
+func testVerifyTransactionStep(tm *openw.WalletManager, rawTx *openwallet.RawTransaction) (*openwallet.RawTransaction, error) {
+
+	//log.Info("rawTx.Signatures:", rawTx.Signatures)
+
+	_, err := tm.VerifyTransaction(testApp, rawTx.Account.WalletID, rawTx.Account.AccountID, rawTx)
+	if err != nil {
+		log.Error("VerifyTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	log.Infof("rawTx: %+v", rawTx)
+	return rawTx, nil
+}
+
+func testSubmitTransactionStep(tm *openw.WalletManager, rawTx *openwallet.RawTransaction) (*openwallet.RawTransaction, error) {
+
+	tx, err := tm.SubmitTransaction(testApp, rawTx.Account.WalletID, rawTx.Account.AccountID, rawTx)
+	if err != nil {
+		log.Error("SubmitTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	log.Std.Info("tx: %+v", tx)
+	log.Info("wxID:", tx.WxID)
+	log.Info("txID:", rawTx.TxID)
+
+	return rawTx, nil
+}
+
+func TestTransfer(t *testing.T) {
+
+	addrs := []string{
+		"03091620fd077333c332b44b187d8a872a52b2b5c1528dda6990cc6c0812e11512",
+		//"03b31a6e7b8256bf83ed97d9ffa76c710203da8fca9bca7f4c597046ec3a424ee1",
+		//"035d5ecf5ed551367aeca8b60ffd68456e4712b930b9be918b03442c99c256f8d1",
+		//"036e471b45022b235c2cba725a322ee430d299ae047a8a7b3f7fb342029a692d2a",
+		//"037406bffed64e5469ba907eb38dfbcedc94faefd20280f3320c3bdb82feb84ebe",
+		//"02e37d5c4b84eb8c002cd339acd23e8329f7d9de2791163b514eaea3bcc03575fa",
+		//"0349cac76201d0e9c6b5904c30de373ddfb7745aab867fb17355efb64152f23c91",
+		//"03453a263a5321a83f1386a97529992a982552f4f10439593ce051076149776b11",
+		//"0256da0db7209272c4e1b0e1e63917eda531aff7b9bfc67e00fee30f03f388fcbf",
+		//"03f2e4cef427fbf35167a606f562e88f9ab22157d198bb3504a0e99c6c3ce9adab",
+	}
+
+	tm := testInitWalletManager()
+	walletID := "WN2r6tixt7V3aSs8DrKCLG9sRPQs1ozVYn"
+	accountID := "DGBwXnyGRXvyvv4SL24xExKhSHsTTdkK6psM7k4akjy3"
+
+	testGetAssetsAccountBalance(tm, walletID, accountID)
+
+	for _, to := range addrs {
+
+		rawTx, err := testCreateTransactionStep(tm, walletID, accountID, to, "1", "", nil)
+		if err != nil {
+			return
+		}
+
+		_, err = testSignTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testVerifyTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testSubmitTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func TestSummary(t *testing.T) {
+	tm := testInitWalletManager()
+	walletID := "WN2r6tixt7V3aSs8DrKCLG9sRPQs1ozVYn"
+	accountID := "2ZnRaccWGRB5KsmrqJkzZy7Gz41CzzV6u8fkXir5oQ2N"
+	summaryAddress := "03a94747ce9fb236b90298cd7bdb7fe71b9183032e9706d548ef64059ca1714c29"
+
+	testGetAssetsAccountBalance(tm, walletID, accountID)
+
+	rawTxArray, err := testCreateSummaryTransactionStep(tm, walletID, accountID,
+		summaryAddress, "", "", "",
+		0, 100, nil)
+	if err != nil {
+		log.Errorf("CreateSummaryTransaction failed, unexpected error: %v", err)
+		return
+	}
+
+	//执行汇总交易
+	for _, rawTx := range rawTxArray {
+		_, err = testSignTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testVerifyTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testSubmitTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+	}
+
+}
